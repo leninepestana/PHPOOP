7bbabf997767f4512833b885854b96a5c2bd5884
leninepestana <leninepestanana@gmail.com>
2020-01-03 10:54:05 +0000

    Section 5: OOP & Databases - PDO Database Class and Connection

---
+++ classes/Database.php
@@ -0,0 +1,36 @@
+<?php
+class Database {
+    private $host	= 'localhost';
+	private $user	= 'root';
+	private $pass	= 'root';
+	private $dbname	= 'myblog_db';
+
+    private $dbh;
+	private $error;
+	private $stmt;
+
+    public function __construct() {
+        // Set DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        // Set Options
+        /*
+        $options = array(
+            PDO::ATTR_PERSISTENT     => true,
+            PDO::ATTR_ERRMODE        => PDO::ERRMODE_EXCEPTION
+        );
+        */
+        $options = array(
+			PDO::ATTR_PERSISTENT		=> true,
+			PDO::ATTR_ERRMODE		=> PDO::ERRMODE_EXCEPTION
+        );
+              
+        // Create new PDO
+       try {
+           $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+       } catch(PDOEception $e) {
+           $this->error = $e->getMessage();
+       }
+
+    }
+}
+?>
