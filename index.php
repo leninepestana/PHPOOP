49a61976520ef9851b11a4d95b0fa2af4b1e9e5b
leninepestana <leninepestana@gmail.com>
2020-01-03 20:55:22 +0000

    Section 5: OOP & Databases - Fetching Data

--- index.php
+++ index.php
@@ -3,4 +3,27 @@ require 'classes/Database.php';
 
 $database = new Database;
 
+//$database->query('SELECT * FROM posts');
+
+$database->query('SELECT * FROM posts WHERE id = :id');
+$database->bind(':id', 1);
+
+$rows = $database->resultset();
+
+//print_r($rows);
+/*
+foreach ($rows as $row) {
+    echo '<h1>' . $row['title'] . '</h1>';
+    echo '<p>' . $row['body'] . '</p>';
+}
+*/
 ?>
+<h1>Posts</h1>
+<div>
+    <?php foreach ($rows as $row) : ?>
+        <div>
+            <h3><?php echo $row['title']; ?></h3>
+            <p><?php echo $row['body']; ?></p>
+        </div>
+    <?php endforeach; ?>
+</div>
