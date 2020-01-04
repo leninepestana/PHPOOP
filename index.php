bd10819a27f43bf87d1c723d5aa805afde3a61b4
leninepestana <leninepestanana@gmail.com>
2020-01-04 02:25:34 +0000

    Section 5: OOP & Databases - Inserting Data

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require 'classes/Database.php';
 
 $database = new Database;
 
-//$database->query('SELECT * FROM posts');
+$database->query('SELECT * FROM posts');
 
-$database->query('SELECT * FROM posts WHERE id = :id');
+//$database->query('SELECT * FROM posts WHERE id = :id');
 $database->bind(':id', 1);
 
 $rows = $database->resultset();
@@ -17,7 +17,37 @@ foreach ($rows as $row) {
     echo '<p>' . $row['body'] . '</p>';
 }
 */
+
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+if ($post['submit']) {
+    $title = $post['title'];
+    $body = $post['body'];
+    
+    $database->query('INSERT INTO posts (title, body) VALUES(:title, :body)');
+    $database->bind(':title', $title);
+    $database->bind(':body', $body);
+   
+    $database->execute();
+
+    
+    if ($database->lastInsertId()) {
+        echo '<p>Post added!</p>';
+    }
+
+    $database->query('SELECT * FROM posts');
+    $rows = $database->resultset();
+    
+}
 ?>
+<h1>Add Post</h1>
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+    <label>Post Title</label><br/>
+    <input type="text" name="title" placeholder="Insert title"><br/>
+    <label>Post Body</label><br/>
+    <textarea name="body"></textarea><br/>
+    <input type="submit" name="submit" value="Submit">
+</form>
 <h1>Posts</h1>
 <div>
     <?php foreach ($rows as $row) : ?>
---
+++ myblog_db.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 03, 2020 at 11:35 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myblog_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `create_date`) VALUES
+(1, 'My Post One', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n', '2020-01-03 23:32:03'),
+(2, 'My Post two', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n', '2020-01-03 23:32:03'),
+(3, 'My Post Three', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n', '2020-01-03 23:33:36'),
+(4, 'My Post Four', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '2020-01-03 23:33:36');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
